cdcf42f15c57752bfd4a5338a3da6ee7d32f1fc1
tiAcolher <101523528+tiAcolher@users.noreply.github.com>
2022-03-13 22:19:33 -0300

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var server_1 = __importDefault(require("./server"));
-// server.listen(process.env.PORT || 5000);
-server_1.default.listen(80);
+server.listen(process.env.PORT || 5000);
+//server_1.default.listen(80);
 //# sourceMappingURL=index.js.map
